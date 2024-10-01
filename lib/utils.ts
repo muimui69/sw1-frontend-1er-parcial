733b1d75f8dbc30826b8d091ea321812de4a2409
muimui69 <moises_c2015@outlook.es>
2024-10-01 18:14:50 -0400

    .

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+import { type ClassValue, clsx } from 'clsx'
+// @ts-ignore
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
